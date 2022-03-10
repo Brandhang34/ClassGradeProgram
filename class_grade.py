1c90fefc70a435f3f52070bbd60b190b9a86ca1f
Brandon Hang <brandonhang34@outlook.com>
2022-03-09 18:51:14 -0800

    refactor code using loop

--- class_grade.py
+++ class_grade.py
@@ -1,22 +1,70 @@
+'''
+**********English**********
+Start
+create a list to store 5 numbers (decimal)
+capture user's input 5 times for their grades
+each time we capture the user's input, we append the number to the list
+sort the list in ascending order, then splice the item starting at index 2
+once we have 3 highest numbers in the list, we sum them up and divide by 3
+output a message to the user
+
+end
+
+
+**********pseudocode**********
+create list
+
+capture input
+append number to list
+
+capture input
+append number to list
+
+capture input
+append number to list
+
+capture input
+append number to list
+
+capture input
+append number to list
+
+print message to user
+'''
+# grades = []
+
+# grade = input("Enter the first grade: ")
+# grades.append(float(grade))
+
+# grade = input("Enter the second grade: ")
+# grades.append(float(grade))
+
+# grade = input("Enter the third grade: ")
+# grades.append(float(grade))
+
+# grade = input("Enter the fourth grade: ")
+# grades.append(float(grade))
+
+# grade = input("Enter the fifth grade: ")
+# grades.append(float(grade))
+
+# grades.sort()
+# grades = grades[2:]
+# grades = sum(grades)
+# result = grades/3
+
+# print("Average Grade: {0:.2f}%".format(result))
+
+'''
+CODE REFACTOR USING LOOP
+'''
+
 grades = []
 
-grade = input("Enter the first grade: ")
-grades.append(float(grade))
-
-grade = input("Enter the second grade: ")
-grades.append(float(grade))
-
-grade = input("Enter the third grade: ")
-grades.append(float(grade))
-
-grade = input("Enter the fourth grade: ")
-grades.append(float(grade))
-
-grade = input("Enter the fifth grade: ")
-grades.append(float(grade))
+for i in range(5):
+    grades.append(float(input("Enter grade: ")))
 
 grades.sort()
-grades = grades[2:]
-grades = sum(grades)
-result = grades/3
-print("Average Grade: {0:.2f}%".format(result))
+grades = sum(grades[2:])/3
+
+print("Average Grade {0:.2f}%".format(grades))
