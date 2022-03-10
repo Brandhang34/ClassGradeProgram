7b1f5b37a899ba93993cd1d1f04a4f18b19e3609
Brandon Hang <brandonhang34@outlook.com>
2022-03-09 18:58:52 -0800

    class grade program with comments

--- class_grade.py
+++ class_grade.py
@@ -1,3 +1,36 @@
+'''
+**********English**********
+Start
+create a list to store 5 numbers (decimal)
+capture user's input 5 times for their grades
+each time we capture the user's input, we append the number to the list
+sort the list in ascending order, then splice the item starting at index 2
+once we have 3 highest numbers in the list, we sum them up and divide by 3
+output a message to the user
+
+end
+
+
+**********pseudocode**********
+create list
+
+capture input
+append 1st number to list
+
+capture input
+append 2nd number to list
+
+capture input
+append 3rd number to list
+
+capture input
+append 4th number to list
+
+capture input
+append 5th number to list
+
+print message to user
+'''
 grades = []
 
 grade = input("Enter the first grade: ")
@@ -19,4 +52,5 @@ grades.sort()
 grades = grades[2:]
 grades = sum(grades)
 result = grades/3
+
 print("Average Grade: {0:.2f}%".format(result))
